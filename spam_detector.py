55d0354aff220a512006b2a56780a9385aa55401
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-04-26 17:17:08 -0400

    code cleanup

--- spam_detector.py
+++ spam_detector.py
@@ -5,39 +5,36 @@
 
 
 import math
-import re
-import sys
-import matplotlib.pyplot as plt 
 import os
+import re
+import matplotlib.pyplot as plt
 import numpy as np
-
 
 # In[10]:
 
 
 train_data_directory = "train/"
 
-
 # ### Word Count
 
 # In[11]:
 
 
-ham_dictionary = {}    # stores words in ham files and their frequencies
-spam_dictionary = {}    # stores words in spam files and their frequencies
-vocabulary = set()    # stores unique words present in all files (spam and ham)
+ham_dictionary = {}  # stores words in ham files and their frequencies
+spam_dictionary = {}  # stores words in spam files and their frequencies
+vocabulary = set()  # stores unique words present in all files (spam and ham)
+
 
 def word_count_directory(train_data_directory):
-    
     # list of file paths for files in train_data_directory
-    file_list = [os.path.join(train_data_directory,f) for f in os.listdir(train_data_directory)]
-    
+    file_list = [os.path.join(train_data_directory, f) for f in os.listdir(train_data_directory)]
+
     # intialize no of spam and ham files
     no_of_spam_files = 0
     no_of_ham_files = 0
-    
+
     for file_path in file_list:
-        with open(file_path,encoding='latin-1') as infile:
+        with open(file_path, encoding='latin-1') as infile:
             # to store type of file 'spam' or 'ham'
             file_type = ''
             if 'spam' in file_path:
@@ -46,15 +43,15 @@ def word_count_directory(train_data_directory):
             elif 'ham' in file_path:
                 file_type = 'ham'
                 no_of_ham_files += 1
-                
+
             # Loop through each line of the file 
             for line in infile:
-                 
-                line = line.strip()    # Remove the leading spaces and newline character
-                lower_line = str.lower(line)    # Convert characters in line to lowercase to avoid case mismatch
-                valid_words = re.split('[^a-zA-Z]',lower_line) # filter words following the given regex
-                valid_words = list(filter(None, valid_words))   # filter words with length greater than 0
-                
+
+                line = line.strip()  # Remove the leading spaces and newline character
+                lower_line = str.lower(line)  # Convert characters in line to lowercase to avoid case mismatch
+                valid_words = re.split('[^a-zA-Z]', lower_line)  # filter words following the given regex
+                valid_words = list(filter(None, valid_words))  # filter words with length greater than 0
+
                 # Iterate over each word in line 
                 for word in valid_words:
                     if file_type == 'ham':
@@ -62,8 +59,8 @@ def word_count_directory(train_data_directory):
                         if word in ham_dictionary:
                             ham_dictionary[word] += 1
                         else:
-                            ham_dictionary[word] = 1     # add word to dictionary with count 1
-                            vocabulary.add(word)     # add word to vocabulary set
+                            ham_dictionary[word] = 1  # add word to dictionary with count 1
+                            vocabulary.add(word)  # add word to vocabulary set
 
                             # if this word is not present in spam_dictionary, add it with count 0
                             if word not in spam_dictionary:
@@ -74,13 +71,13 @@ def word_count_directory(train_data_directory):
                         if word in spam_dictionary:
                             spam_dictionary[word] += 1
                         else:
-                            spam_dictionary[word] = 1    # add word to dictionary with count 1
-                            vocabulary.add(word)    # add word to vocabulary set
+                            spam_dictionary[word] = 1  # add word to dictionary with count 1
+                            vocabulary.add(word)  # add word to vocabulary set
 
                             # if this word is not present in ham_dictionary, add it with count 0
                             if word not in ham_dictionary:
                                 ham_dictionary[word] = 0
-    return no_of_spam_files,no_of_ham_files
+    return no_of_spam_files, no_of_ham_files
 
 
 # ### Prior Probabilities
@@ -99,37 +96,37 @@ prior_prob_of_ham = no_of_ham_files / total_no_of_files
 # In[13]:
 
 
-def create_model(vocabulary,ham_dictionary,spam_dictionary):
-    
-    vocabulary = sorted(vocabulary)    # sorting the vocabulary to maintain order in model.txt
-    f = open("model.txt","w+")    # creating file that would store the model
-    N = len(vocabulary)    # getting size of vocabulary
-    delta = 0.5    # smoothing value
-    
+def create_model(vocabulary, ham_dictionary, spam_dictionary):
+    vocabulary = sorted(vocabulary)  # sorting the vocabulary to maintain order in model.txt
+    f = open("model.txt", "w+")  # creating file that would store the model
+    N = len(vocabulary)  # getting size of vocabulary
+    delta = 0.5  # smoothing value
+
     smoothed_N = (delta * N)
     # calculating smoothed denominator for calculating condinational probability of ham words
     ham_denominator = sum(ham_dictionary.values()) + smoothed_N
-    
+
     # calculating smoothed denominator for calculating condinational probability of spam words
     spam_denominator = sum(spam_dictionary.values()) + smoothed_N
-    
-    for i,word in enumerate(vocabulary):
-        
-        freq_in_ham = ham_dictionary[word]    # frequency of word in ham dictionary
-        c_p_in_ham = (freq_in_ham + delta) / ham_denominator    # conditional probabiltiy of word in ham
-        freq_in_spam = spam_dictionary[word]    # frequency of word in spam dictionary
-        c_p_in_spam = (freq_in_spam + delta) / spam_denominator    # conditional probabiltiy of word in spam
-        
+
+    for i, word in enumerate(vocabulary):
+        freq_in_ham = ham_dictionary[word]  # frequency of word in ham dictionary
+        c_p_in_ham = (freq_in_ham + delta) / ham_denominator  # conditional probabiltiy of word in ham
+        freq_in_spam = spam_dictionary[word]  # frequency of word in spam dictionary
+        c_p_in_spam = (freq_in_spam + delta) / spam_denominator  # conditional probabiltiy of word in spam
+
         ham_dictionary[word] = c_p_in_ham
         spam_dictionary[word] = c_p_in_spam
-        
-        # writing all data to model.txt
-        f.write(str(i+1)+'  '+word+'  '+str(freq_in_ham)+'  '+str( "{:.8f}".format(float( c_p_in_ham )) )+'  '+str(freq_in_spam)+'  '+str( "{:.8f}".format(float( c_p_in_spam )) )+'\n')
-    
-    f.close()    # closing the file
-    
-create_model(vocabulary,ham_dictionary,spam_dictionary)
 
+        # writing all data to model.txt
+        f.write(str(i + 1) + '  ' + word + '  ' + str(freq_in_ham) + '  ' + str(
+            "{:.8f}".format(float(c_p_in_ham))) + '  ' + str(freq_in_spam) + '  ' + str(
+            "{:.8f}".format(float(c_p_in_spam))) + '\n')
+
+    f.close()  # closing the file
+
+
+create_model(vocabulary, ham_dictionary, spam_dictionary)
 
 # ### Evaluate the model
 
@@ -143,40 +140,38 @@ log_of_ham = math.log10(prior_prob_of_ham)
 log_of_spam = math.log10(prior_prob_of_spam)
 
 # initialising variables needed for confusion matrix
-true_positive = 0   # correct Ham -> result Ham
-true_negative = 0    # correct Spam -> result Spam
-false_positive = 0    # correct Spam -> result Ham
-false_negative = 0    # correct Ham -> result Spam
+true_positive = 0  # correct Ham -> result Ham
+true_negative = 0  # correct Spam -> result Spam
+false_positive = 0  # correct Spam -> result Ham
+false_negative = 0  # correct Ham -> result Spam
 
-file_list = [os.path.join(test_data_directory,f) for f in os.listdir(test_data_directory)]    # file paths of test files
-temp_counter = 0    # counter to store the test file count
+file_list = [os.path.join(test_data_directory, f) for f in os.listdir(test_data_directory)]  # file paths of test files
+temp_counter = 0  # counter to store the test file count
 
-f = open("result.txt", "w+")   # 'w+' for reading and writing
+f = open("result.txt", "w+")  # 'w+' for reading and writing
 f.truncate(0)
 
 for file_path in file_list:
-    
-    with open(file_path,encoding = 'latin-1') as infile:
 
-        file_name = file_path.rsplit('/',1)[1]    # file name to store in result.txt
+    with open(file_path, encoding='latin-1') as infile:
+
+        file_name = file_path.rsplit('/', 1)[1]  # file name to store in result.txt
         temp_counter = temp_counter + 1
-        score_log_ham = log_of_ham     # score for ham
-        score_log_spam = log_of_spam    # score for spam
+        score_log_ham = log_of_ham  # score for ham
+        score_log_spam = log_of_spam  # score for spam
 
-        if("test-ham" in file_path):
+        if "test-ham" in file_path:
             correct_classification = "ham"
         else:
             correct_classification = "spam"
 
-        vocab_test = []    #  stores words in test file
+        vocab_test = []  # stores words in test file
         for line in infile:
-
-            line = line.strip()    # Remove the leading spaces and newline character
-            lower_line = str.lower(line)    # Convert characters in line to lowercase to avoid case mismatch
-            valid_words = re.split('[^a-zA-Z]',lower_line) # filter words following the given regex
-            valid_words = list(filter(None, valid_words))   # filter words with length greater than 0
-            vocab_test = vocab_test + valid_words    # appending valid_words to vocab_test
-
+            line = line.strip()  # Remove the leading spaces and newline character
+            lower_line = str.lower(line)  # Convert characters in line to lowercase to avoid case mismatch
+            valid_words = re.split('[^a-zA-Z]', lower_line)  # filter words following the given regex
+            valid_words = list(filter(None, valid_words))  # filter words with length greater than 0
+            vocab_test = vocab_test + valid_words  # appending valid_words to vocab_test
 
         for word in vocab_test:
             if word in vocabulary:
@@ -184,38 +179,38 @@ for file_path in file_list:
                 score_log_ham = score_log_ham + math.log10(ham_dictionary[word])
                 # add log10 of conditional probability of word in ham_dictionary
                 score_log_spam = score_log_spam + math.log10(spam_dictionary[word])
-        
 
-        if(score_log_ham > score_log_spam):
+        if score_log_ham > score_log_spam:
             predicted_classification = "ham"
         else:
             predicted_classification = "spam"
 
-        if(correct_classification == predicted_classification):
+        if correct_classification == predicted_classification:
             label = "right"
         else:
             label = "wrong"
 
-        if(correct_classification == "ham" and predicted_classification == "ham"):
+        if correct_classification == "ham" and predicted_classification == "ham":
             true_positive = true_positive + 1
-            
-        elif(correct_classification == "spam" and predicted_classification == "spam"):
+
+        elif correct_classification == "spam" and predicted_classification == "spam":
             true_negative = true_negative + 1
-            
-        elif(correct_classification == "spam" and predicted_classification == "ham"):
+
+        elif correct_classification == "spam" and predicted_classification == "ham":
             false_positive = false_positive + 1
-            
-        elif(correct_classification == "ham" and predicted_classification == "spam"):
+
+        elif correct_classification == "ham" and predicted_classification == "spam":
             false_negative = false_negative + 1
 
         # format scores to appropriate string value
-        score_log_ham = str( "{:.8f}".format(float(score_log_ham)))
-        score_log_spam = str( "{:.8f}".format(float(score_log_spam)))
-        
-        # writing results to result.txt
-        f.write(str(str(temp_counter)+" "+str(file_name)+" "+str(predicted_classification)+" "+str(score_log_ham)+" "+str(score_log_spam)+" "+str(correct_classification)+" "+str(label)+"\n"))
+        score_log_ham = str("{:.8f}".format(float(score_log_ham)))
+        score_log_spam = str("{:.8f}".format(float(score_log_spam)))
 
-f.close()    # closing file
+        # writing results to result.txt
+        f.write(str(str(temp_counter) + " " + str(file_name) + " " + str(predicted_classification) + " " + str(
+            score_log_ham) + " " + str(score_log_spam) + " " + str(correct_classification) + " " + str(label) + "\n"))
+
+f.close()  # closing file
 
 
 # ### Confusion Matrix
@@ -223,12 +218,10 @@ f.close()    # closing file
 # In[21]:
 
 
+def confusion_matrix(values, class_name, title='Confusion matrix'):
+    label_array = np.array([["(True Positive)", "(False Positive)"],
+                            ["(False Negative)", "(True Negative)"]])
 
-def confusion_matrix(values , class_name , title='Confusion matrix'):
-
-    label_array=np.array([[ "(True Positive)",  "(False Positive)"],
-                                              [  "(False Negative)",  "(True Negative)"]])
-   
     plt.figure(figsize=(4, 4))
     plt.imshow(values)
     plt.title(title)
@@ -237,67 +230,64 @@ def confusion_matrix(values , class_name , title='Confusion matrix'):
     plt.xticks(tick, class_name)
     plt.yticks(tick, class_name)
 
-
-    for i in range (values.shape[0]):
-        for j in range (values.shape[1]):    
+    for i in range(values.shape[0]):
+        for j in range(values.shape[1]):
             plt.text(j, i, "{:,}".format(values[i, j]),
-            color="red",horizontalalignment="center",verticalalignment="bottom")
-            plt.text(j, i,label_array[i,j],
-            color="red",horizontalalignment="center",verticalalignment="top")
-
+                     color="red", horizontalalignment="center", verticalalignment="bottom")
+            plt.text(j, i, label_array[i, j],
+                     color="red", horizontalalignment="center", verticalalignment="top")
 
     plt.tight_layout()
     plt.ylabel('Predicted ')
-    plt.xlabel('Actual ')     
+    plt.xlabel('Actual ')
     plt.show()
 
 
 # In[22]:
 
 
-confusion_matrix(values= np.array([[ true_positive, false_positive ],
-                                    [  false_negative,  true_negative]]), class_name = ['Ham', 'Spam'],title="Confusion matrix for Ham Class")
+confusion_matrix(values=np.array([[true_positive, false_positive],
+                                  [false_negative, true_negative]]), class_name=['Ham', 'Spam'],
+                 title="Confusion matrix for Ham Class")
 
-
-confusion_matrix(values= np.array([[ true_negative , false_negative ],
-                                    [  false_positive,  true_positive]]), class_name = ['Spam', 'Ham'],title="Confusion matrix for Spam class")
-
+confusion_matrix(values=np.array([[true_negative, false_negative],
+                                  [false_positive, true_positive]]), class_name=['Spam', 'Ham'],
+                 title="Confusion matrix for Spam class")
 
 # ### Evaluation Metrics
 
 # In[23]:
 
 
-accuracy_ham = true_positive/(true_positive+false_negative)*100
-accuracy_spam = true_negative/(true_negative+false_positive)*100
+accuracy_ham = true_positive / (true_positive + false_negative) * 100
+accuracy_spam = true_negative / (true_negative + false_positive) * 100
 
-accuracy_model=(true_positive+true_negative)/(true_positive+true_negative+false_positive+false_negative)*100
+accuracy_model = (true_positive + true_negative) / (
+            true_positive + true_negative + false_positive + false_negative) * 100
 
-precision_ham = true_positive/(true_positive+false_positive)
-precision_spam =  true_negative/(true_negative+false_negative)
+precision_ham = true_positive / (true_positive + false_positive)
+precision_spam = true_negative / (true_negative + false_negative)
 
-recall_ham =   true_positive/ (true_positive + false_negative)
-recall_spam =  true_negative/(true_negative+false_positive)
+recall_ham = true_positive / (true_positive + false_negative)
+recall_spam = true_negative / (true_negative + false_positive)
 
-F1measure_ham =  2*(recall_ham * precision_ham) / (recall_ham + precision_ham)
-F1measure_spam = 2*(recall_spam * precision_spam) / (recall_spam + precision_spam)
-
+F1measure_ham = 2 * (recall_ham * precision_ham) / (recall_ham + precision_ham)
+F1measure_spam = 2 * (recall_spam * precision_spam) / (recall_spam + precision_spam)
 
 # In[24]:
 
 
-data = [[ "Ham", accuracy_ham,  precision_ham, recall_ham,  F1measure_ham],
-        [ "Spam", accuracy_spam,  precision_spam,  recall_spam, F1measure_spam],
-        [ "Model", accuracy_model,  "",""  ,"" ]]
+data = [["Ham", accuracy_ham, precision_ham, recall_ham, F1measure_ham],
+        ["Spam", accuracy_spam, precision_spam, recall_spam, F1measure_spam],
+        ["Model", accuracy_model, "", "", ""]]
 
-fig, axs =plt.subplots(2,1)
-collabel=("Class","Accuracy", "Precision", "Recall","F1-measure")
+fig, axs = plt.subplots(2, 1)
+col_label = ("Class", "Accuracy", "Precision", "Recall", "F1-measure")
 axs[0].axis('tight')
 axs[0].axis('off')
 axs[1].axis("off")
-the_table = axs[0].table(cellText=data,colLabels=collabel,loc='center')
+the_table = axs[0].table(cellText=data, colLabels=col_label, loc='center')
 the_table.auto_set_font_size(False)
 the_table.set_fontsize(10)
 the_table.scale(2, 2)
 plt.show()
-
